e1e154aca2758b43013c42a2dd4aafd51c8085a1
Vinicios Wentz <wentz.vinicios@gmail.com>
2023-05-05 15:14:04 -0300

    mix.exs and mix.lock added

---
+++ mix.exs
@@ -0,0 +1,47 @@
+defmodule ElixirDockerBBuilder.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :elixir_docker_builder,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      release_date: ~D[2023-02-16],
+      compilers: [:domo_compiler] ++ Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  defp deps do
+    [
+      {:phoenix, "~> 1.7.1", override: true},
+      {:phoenix_ecto, "~> 4.4"},
+      {:ecto_sql, "~> 3.6"},
+      {:postgrex, ">= 0.0.0"},
+      {:ecto_erd, "~> 0.5.0", only: :dev},
+      {:swoosh, "~> 1.3"},
+      {:finch, "~> 0.13"},
+      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_poller, "~> 1.0"},
+      {:gettext, "~> 0.18"},
+      {:acx, git: "https://github.com/casbin/casbin-ex"},
+      {:jason, "~> 1.2"},
+      {:faker, "~> 0.17"},
+      {:ex_machina, "~> 2.7.0", only: :test},
+      {:junit_formatter, "~> 3.3", only: :test},
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:canary, "~> 1.1.1"},
+      {:money, "~> 1.12"},
+      {:mix_audit, "~> 2.0", only: [:dev, :test], runtime: false},
+      {:oban, "~> 2.14.2"},
+      {:joken, "~> 2.6"},
+      {:paper_trail, "~> 1.0.0"},
+      {:tesla, "~> 1.5"},
+      {:hackney, "~> 1.10"},
+      {:inflex, "~> 2.0.0"},
+      {:domo, "~> 1.5"}
+    ]
+  end
+end
